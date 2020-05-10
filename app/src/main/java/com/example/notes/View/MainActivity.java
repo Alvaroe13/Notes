813c02e99f5adf8b0a270716063f818e4801a02f
Alvaro Isea <alvaroe9448@gmail.com>
2020-05-10 19:43:35 +0200

    *added floating action button *change layout design

--- app/src/main/java/com/example/notes/View/MainActivity.java
+++ app/src/main/java/com/example/notes/View/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.notes.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.notes.Model.Note;
 import com.example.notes.Model.NoteRepository;
@@ -15,6 +18,7 @@ import com.example.notes.R;
 import com.example.notes.View.Adapter.NoteAdapter;
 import com.example.notes.ViewModel.NoteViewModel;
 import com.example.notes.ViewModel.NoteViewModelFactory;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -23,16 +27,43 @@ public class MainActivity extends AppCompatActivity {
 
      private NoteViewModel noteViewModel;
      private RecyclerView recyclerView;
-     private  NoteAdapter adapter;
+     private NoteAdapter adapter;
+     //ui
+    private FloatingActionButton addNoteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addNoteButton = findViewById(R.id.addNoteButton);
+
+        setToolbar();
         Recycler();
         initViewModel();
+        fabButton();
 
+    }
+
+    private void fabButton() {
+        addNoteButton = findViewById(R.id.addNoteButton);
+        addNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToNoteRoom();
+            }
+        });
+    }
+
+    private void goToNoteRoom() {
+        Intent i = new Intent(this, NoteRoomActivity.class);
+        startActivity(i);
+    }
+
+    private void setToolbar(){
+        Toolbar toolbar = findViewById(R.id.noteToolbarTitle);
+        setSupportActionBar(toolbar);
+        setTitle("Notes");
     }
 
     private void Recycler(){
